ff07c6c00368be829c5dffe39145c3732db05e31
Judah Lomo <judahllomo@gmail.com>
2025-01-31 13:44:30 -0600

    Initial Commit

---
+++ TipCalculatorApp/TipCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorAppApp.swift
+//  TipCalculatorApp
+//
+//  Created by Judah Lomo on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
